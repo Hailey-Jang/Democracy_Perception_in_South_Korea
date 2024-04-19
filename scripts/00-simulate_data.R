4249568390e8afb080274233cbf953a7a2b7bf3d
Hailey-Jang <hari21315@gmail.com>
2024-04-18 21:26:19 -0400

    Update files after the few changes from data simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -13,21 +13,21 @@ library(dplyr)
 country_names <- c("Republic of Korea") 
 genders <- c(1, 2)  # 1 for Male, 2 for Female
 satisfaction_levels <- 1:5
-corruption_levels <- 1:8
+Distrusts_levels <- 1:5
 political_levels <- 1:5
-economic_levels <- 1:8
+economic_levels <- 1:4
 age_range <- 1931:1997
 
 # Function to simulate data
-simulate_data <- function(n) {
+simulated_data <- function(n) {
   data_frame(
     Country_Name = sample(country_names, n, replace = TRUE),
     Gender = sample(genders, n, replace = TRUE),
     Age = sample(age_range, n, replace = TRUE),
-    Satisfaction_With_Democracy = sample(satisfaction_levels, n, replace = TRUE),
-    Corruption_Perception = sample(corruption_levels, n, replace = TRUE),
-    Political_Perception = sample(political_levels, n, replace = TRUE),
-    Economic_Perception = sample(economic_levels, n, replace = TRUE)
+    Dissatisfaction_with_democracy = sample(satisfaction_levels, n, replace = TRUE),
+    Distrust_level_agianst_politician = sample(Distrusts_levels, n, replace = TRUE),
+    Dissatisfaction_with_politics = sample(political_levels, n, replace = TRUE),
+    Dissatisfaction_with_economy = sample(economic_levels, n, replace = TRUE)
   )
 }
 
